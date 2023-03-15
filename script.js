521e03591d0b23772c1654ab2d27cc909b72c0c2
PierreFrs <pierrefraisse90@gmail.com>
2023-03-15 15:41:03 +0100

    Volume slider

--- script.js
+++ script.js
@@ -47,8 +47,38 @@ function updateProgress() {
     duration.textContent = `${displayTime(video.duration)}`;
 }
 
+// Click to seek within the video
+function setProgress(e) {
+    const newTime = (e.offsetX / progressRange.offsetWidth);
+    progressBar.style.width = `${newTime * 100}%`
+    video.currentTime = newTime * video.duration;
+    console.log(newTime);
+}
+
 // Volume Controls --------------------------- //
 
+// Volume Bar
+function changeVolume(e) {
+    let volume = e.offsetX / volumeRange.offsetWidth;
+    // Rounding volume up or down
+    if (volume < 0.1) {
+        volume = 0;
+    }
+    if (volume > 0.9) {
+        volume = 1;
+    }
+    volumeBar.style.width = `${volume * 100}%`;
+    video.volume = volume;
+    // Change Icon depending on volume
+    volumeIcon.className = '';
+    if (volume > 0.7) {
+        volumeIcon.classList.add('fa-solid', 'fa-volume-high');
+    } else if (volume < 0.7 && volume > 0) {
+        volumeIcon.classList.add('fa-solid', 'fa-volume-low')
+    } else if (volume === 0) {
+        volumeIcon.classList.add('fa-solid', 'fa-volume-xmark');
+    }
+}
 
 
 // Change Playback Speed -------------------- //
@@ -63,3 +93,5 @@ playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
 video.addEventListener('timeupdate', updateProgress);
 video.addEventListener('canplay', updateProgress);
+progressRange.addEventListener('click', setProgress)
+volumeRange.addEventListener('click', changeVolume)
